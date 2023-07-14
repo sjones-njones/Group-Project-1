a0ba8a05b1baed40ee633daa11ca6fd3bece67eb
lcurtis0 <133440522+lcurtis0@users.noreply.github.com>
2023-07-14 14:52:12 -0400

    getting layout for for loop (images)

--- assets/js/thirdPage.js
+++ assets/js/thirdPage.js
@@ -30,52 +30,62 @@ if statment to print photo
 have addeventListner to the button to contiue the array length and 
 display info i.e. address, street name, zip etc
 
-
-
-
-function{
-
-var houseBitCoinValue = localStorage.getItem()
-
-console.log(houseBitCoinValue);
-
-houseBitCoinValue.
-
-}
-
 */
 
-fetch('https://zillow-com1.p.rapidapi.com/propertyExtendedSearch?location=' + addressEl + '&home_type=Houses', {
+var pictureHere = document.querySelector('.Picture-here')
+
+var formSubmitHandler = function () {
+  /*
+  var streetEl = $("#street-address").val().trim();
+  var aptEl = $("#apt-number").val().trim();
+  var cityEl = $("#city-address").val().trim();
+  var stateEl = $("#state-select").val().trim();
+  var zipEl = $("#zip-address").val().trim();
+ var addressEl = streetEl + aptEl + " " + cityEl + " " + stateEl + " " + zipEl
+ */
+ var addressEl = 'atlantaGeorgia';
+  useAddress(addressEl);
+}
+formSubmitHandler();
+//patuxentAve.BroomesIslandMaryland20615
+
+// uses user address to get zpid
+function useAddress(addressEl) {
+  fetch('https://zillow-com1.p.rapidapi.com/propertyExtendedSearch?location=' + addressEl + '&home_type=Houses', {
     headers: {
       'X-RapidAPI-Key': '0e88e3b544msh53627318f7da7a0p18e841jsn5dd8f250f5f6',
       'X-RapidAPI-Host': 'zillow-com1.p.rapidapi.com'
     }
   })
     .then(function (response) {
-      return response.json()
+      return response.json();
     })
     .then(function (data) {
-      var zpidEl = data.zpid;
-      getValue(zpidEl);
-    });
+      console.log(data);
+      console.log(data.props[12].zpid);
 
-/*
-function getBitcoinVal(){
-    fetch("https://rest.coinapi.io/v1/exchangerate/BTC/USD", {
-      headers: { 'X-CoinAPI-Key' : '406DA5B8-4FA9-4947-81FE-5A06619B3BB3' }
-    })
-      .then(function (response) {
-        return response.json()
-      })
-      .then(function (data) {
-        var rateEl = data.rate.toFixed(2);
-        console.log (zestimateEl);
-        console.log (rateEl);
-        bcValue = zestimateEl/rateEl;
-        console.log(bcValue);
-      });
+      for(i=0; i < 8; i++){
+
+      var imageHouse = document.createElement('p');
+      imageHouse.textContent = data.props[i].imgScr;
+      pictureHere.append(data.props[i].imgScr);
+
+      var addressInfo = document.createElement('p');
+      addressInfo.textContent = data.props[i].zpid;
+      pictureHere.append(data.props[i].zpid);
+
       }
 
-      getBitcoinVal();
+    });
+}
 
-      */
+//https://zillow-com1.p.rapidapi.com/propertyExtendedSearch?location=patuxentAve.BroomesIslandMaryland20615&home_type=Houses0e88e3b544msh53627318f7da7a0p18e841jsn5dd8f250f5f6
+
+//var keyword = getitem(keyword);
+
+//keyword.data.props[i].imgScr
+
+// data recieved should be a list of addresses ~= to bitcoin value
+
+// fetch api request for images of houses
+
