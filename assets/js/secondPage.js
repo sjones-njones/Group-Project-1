4aba34a47c3150268092aa73997792dbe0d891a1
stephanie jones <sjones.njones@gmail.com>
2023-07-17 08:08:32 -0500

    got button back and added address to 2nd page

--- assets/js/secondPage.js
+++ assets/js/secondPage.js
@@ -11,6 +11,8 @@ console.log(myHouseEl);
 (bitcoinValueEl).text(myHouseEl.bitcoinHouseValue + " bitcoins");
 var pictureEl = $("#picture");
 $(pictureEl).attr("src", myHouseEl.pic);
+var addressString = myHouseEl.address.streetAddress + " " + myHouseEl.address.city + " " + myHouseEl.address.state + " " + myHouseEl.address.zipcode;
+$(".address").text(addressString);
  }
 
 
